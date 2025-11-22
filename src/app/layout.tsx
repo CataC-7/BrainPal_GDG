680bb6d4be556422df1b6b1969dd0d90e8050f81
Catalina Cepeda <catacepe@gmail.com>
2025-11-22 15:24:22 +0000

    Amazing, and one last change. The tab and this project is call RoutineRe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'RoutineRecall',
+  title: 'BrainPal',
   description:
     'Easily store and access your daily workflows and emergency interventions.',
 };
